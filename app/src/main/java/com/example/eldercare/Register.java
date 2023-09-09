8617a27866701ae0107b10fcf72d3965dd6bcf05
Anders Ullström <zefaniasmusic@gmail.com>
2023-09-09 15:40:32 +0200

    Inital code for project with simple login/register system with Firebase authenticator & realtime database.

---
+++ app/src/main/java/com/example/eldercare/Register.java
@@ -0,0 +1,103 @@
+package com.example.eldercare;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Register extends AppCompatActivity {
+
+    private TextInputEditText editTextEmail, editTextFirstName, editTextLastName, editTextPhoneNumber, editTextPassword;
+    private Button registerButton;
+    private ProgressBar progressBar;
+    private TextView textView;
+    private FirebaseAuth mAuth;
+    private DatabaseReference usersRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        usersRef = FirebaseDatabase.getInstance().getReference().child("users");
+
+        editTextEmail = findViewById(R.id.email);
+        editTextFirstName = findViewById(R.id.firstname);
+        editTextLastName = findViewById(R.id.lastname);
+        editTextPhoneNumber = findViewById(R.id.phonenumber);
+        editTextPassword = findViewById(R.id.password);
+        registerButton = findViewById(R.id.registerButton);
+        progressBar = findViewById(R.id.progressBar);
+        textView = findViewById(R.id.loginNow);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+    }
+
+    private void registerUser() {
+        progressBar.setVisibility(View.VISIBLE);
+        String email = editTextEmail.getText().toString().trim();
+        String firstName = editTextFirstName.getText().toString().trim();
+        String lastName = editTextLastName.getText().toString().trim();
+        String phoneNumber = editTextPhoneNumber.getText().toString().trim();
+        String password = editTextPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(firstName) ||
+                TextUtils.isEmpty(lastName) || TextUtils.isEmpty(phoneNumber) ||
+                TextUtils.isEmpty(password)) {
+            Toast.makeText(Register.this, "All fields are required", Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.GONE);
+            return;
+        }
+
+        // Create a new user with Firebase Authentication
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // User registration successful
+                        String userId = mAuth.getCurrentUser().getUid();
+                        DatabaseReference userReference = usersRef.child(userId);
+                        userReference.child("email").setValue(email);
+                        userReference.child("firstname").setValue(firstName);
+                        userReference.child("lastname").setValue(lastName);
+                        userReference.child("phone-number").setValue(phoneNumber);
+
+                        Toast.makeText(Register.this, "Registration Successful.", Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                        Intent intent = new Intent(getApplicationContext(), Login.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        // Registration failed
+                        Toast.makeText(Register.this, "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
+    }
+}
