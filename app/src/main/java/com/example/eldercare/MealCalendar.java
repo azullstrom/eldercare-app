cdaf252b110ebb971cdd31312796acac72f389af
Oscar Stenqvist <oscar@Oscars-MacBook-Pro-2.local>
2023-09-14 15:40:20 +0200

    Added temp calendar and buttons

--- app/src/main/java/com/example/eldercare/MealCalendar.java
+++ app/src/main/java/com/example/eldercare/MealCalendar.java
@@ -3,6 +3,7 @@ package com.example.eldercare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
