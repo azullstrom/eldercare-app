b9c1043ede8195fdcfbbaa43a67acdd596abebec
Oscar Stenqvist <oscar@Oscars-MacBook-Pro-2.local>
2023-09-14 20:37:45 +0200

    Started work on editing meal

---
+++ app/src/main/java/com/example/eldercare/MealCalendarEdit.java
@@ -0,0 +1,14 @@
+package com.example.eldercare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MealCalendarEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_calendar_edit);
+    }
+}
