f06064d526386f42f5dc028eebfb45cd03302bf3
Anders Ullström <zefaniasmusic@gmail.com>
2023-09-15 20:17:25 +0200

    Lagt till assignElderlyToCaregiver och removeElderlyFromCaregiver och implementerat

---
+++ app/src/main/java/com/example/eldercare/DatabaseLib.java
@@ -0,0 +1,107 @@
+package com.example.eldercare;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.example.eldercare.CaregiverMainActivity;
+import com.example.eldercare.Login;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class DatabaseLib {
+    private DatabaseReference rootRef;
+    private Context context;
+
+    public DatabaseLib(Context context) {
+        this.context = context;
+        rootRef = FirebaseDatabase.getInstance().getReference();
+    }
+    public void assignElderlyToCaregiver(String firstNameElderly, String firstNameCaregiver) {
+        DatabaseReference elderlyRef = rootRef.child("elderly-users").child(firstNameElderly);
+        DatabaseReference caregiverRef = rootRef.child("caregiver-users").child(firstNameCaregiver);
+
+        elderlyRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot elderlySnapshot) {
+                if (elderlySnapshot.exists()) {
+                    caregiverRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot caregiverSnapshot) {
+                            if (caregiverSnapshot.exists()) {
+                                // Assign the elderly to the caregiver by updating caregiver's node
+                                caregiverRef.child("assigned-elderly").child(firstNameElderly).setValue(true);
+                                Toast.makeText(context, "Successfully assigned!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // If the caregiver user doesn't exist
+                                Toast.makeText(context, "Enter valid caregiver.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            // Handle any database errors here
+                        }
+                    });
+                } else {
+                    // If the elderly user doesn't exist
+                    Toast.makeText(context, "Enter valid elderly.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle any database errors here
+            }
+        });
+    }
+
+    public void removeElderlyFromCaregiver(String firstNameElderly, String firstNameCaregiver) {
+        DatabaseReference elderlyRef = rootRef.child("elderly-users").child(firstNameElderly);
+        DatabaseReference caregiverRef = rootRef.child("caregiver-users").child(firstNameCaregiver);
+
+        elderlyRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot elderlySnapshot) {
+                if (elderlySnapshot.exists()) {
+                    caregiverRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot caregiverSnapshot) {
+                            if (caregiverSnapshot.exists()) {
+                                // Remove the elderly from the caregiver's assigned-elderly node
+                                caregiverRef.child("assigned-elderly").child(firstNameElderly).removeValue();
+                                Toast.makeText(context, "Successfully removed!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // If the caregiver user doesn't exist
+                                Toast.makeText(context, "Enter valid caregiver.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            // Handle any database errors here
+                        }
+                    });
+                } else {
+                    // If the elderly user doesn't exist
+                    Toast.makeText(context, "Enter valid elderly.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle any database errors here
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/eldercare/Login.java
+++ app/src/main/java/com/example/eldercare/Login.java
@@ -7,20 +7,17 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.eldercare.libs.DatabaseLib;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
@@ -39,7 +36,7 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        databaseLib = new DatabaseLib();
+        databaseLib = new DatabaseLib(this);
         mAuth = FirebaseAuth.getInstance();
 
         SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
