97f72f756f2f2b6f5fb3bb37ba01951846537be9
henrikhgren <henrikhultgren@protonmail.com>
2023-09-19 12:39:37 +0200

    Fixat nåen dialogruta när man trycker lus Innehåller 2 knappar

--- app/src/main/java/com/example/eldercare/CaregiverMainActivity.java
+++ app/src/main/java/com/example/eldercare/CaregiverMainActivity.java
@@ -1,10 +1,17 @@
 package com.example.eldercare;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,16 +30,60 @@ public class CaregiverMainActivity extends AppCompatActivity {
         ImageView noPatientsImageView = findViewById(R.id.noPatientsImageView);
         TextView noPatientsTextView = findViewById(R.id.noPatientsTextView);
         ImageButton addPatientButton = findViewById(R.id.addImageButton);
+
+
+        //Listener for "plus" ImageButton
         addPatientButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Open new activity (add etc)
-                Intent intent = new Intent(CaregiverMainActivity.this, CaregiverMainActivity.class);
-                startActivity(intent);
+                showAlertDialog();
             }
         });
 
     }
+
+
+    //AlertDialog-method (When pressing plus ImageButton)
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.activity_caregiver_main_dialogbox, null);
+
+        Button addNewElderButton = dialogView.findViewById(R.id.add_new_elder);
+        Button addExistingElderButton = dialogView.findViewById(R.id.add_existing_elder);
+
+        addNewElderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Add new elder
+            }
+        });
+
+        addExistingElderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Add existing elder
+            }
+        });
+
+        builder.setView(dialogView);
+        AlertDialog alertDialog = builder.create();
+
+        //Changes the dimension and position of the dialog box. Feels too over complicated
+        Window window = alertDialog.getWindow();
+        if (window != null) {
+            WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+            layoutParams.copyFrom(window.getAttributes());
+            layoutParams.gravity = Gravity.BOTTOM;
+            layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+            layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+            window.setAttributes(layoutParams);
+        }
+
+        alertDialog.show();
+    }
+
+
 
     protected boolean patientsExists() {
         //If not exists -> Show image, "no patients text" and plus Imagebutton
