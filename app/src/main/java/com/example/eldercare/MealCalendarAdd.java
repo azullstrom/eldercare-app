88d818f1e9faaa662b0eba3fccd4f26e75fce338
Oscar Stenqvist <oscar@Oscars-MacBook-Pro-2.local>
2023-09-22 00:04:38 +0200

    Improved dimming of background when popup appears, improved overall design and started working on adding meals

---
+++ app/src/main/java/com/example/eldercare/MealCalendarAdd.java
@@ -0,0 +1,14 @@
+package com.example.eldercare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MealcalendarAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mealcalendar_add);
+    }
+}
