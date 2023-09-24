9c7b50a6b4e56f81a20c8950aa362b3d522dc110
Ahmad Abo Warda <72770066+Apses98@users.noreply.github.com>
2023-09-24 03:45:45 +0200

    changing Heading name

--- app/src/main/java/com/example/eldercare/CaregiverElderlyOverviewActivity.java
+++ app/src/main/java/com/example/eldercare/CaregiverElderlyOverviewActivity.java
@@ -1,4 +1,26 @@
 package com.example.eldercare;
 
-public class CaregiverElderlyOverviewActivity {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class CaregiverElderlyOverviewActivity extends AppCompatActivity {
+
+    private TextView patientNameTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caregiver_elderly_overview);
+
+        patientNameTextView = findViewById(R.id.patient_name_placeholder);
+        // Expecting to send name and dateOfBirth when anybody calls this activity/class
+        String elderlyName = getIntent().getStringExtra("elderlyName");
+        patientNameTextView.setText(elderlyName);
+
+
+
+    }
 }
