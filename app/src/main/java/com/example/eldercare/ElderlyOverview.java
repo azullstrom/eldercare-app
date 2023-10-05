f0b86524afb2e1525ddc06df8ba5fe22ede894a3
Commando <moutasem.amin@hotmail.com>
2023-10-06 01:20:48 +0200

    Add databse refernce and text view for test

--- app/src/main/java/com/example/eldercare/ElderlyOverview.java
+++ app/src/main/java/com/example/eldercare/ElderlyOverview.java
@@ -1,17 +1,30 @@
 package com.example.eldercare;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ElderlyOverview extends AppCompatActivity {
 
+
+    FirebaseDatabase firebaseDatabase ;
+    DatabaseReference databaseReference;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
          // Set current activity to ac
         setContentView(R.layout.activity_elderly);
+        textView.findViewById(R.id.textViewTest);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("elderly-users").child("Dag1930").child("meals");
 
     }
+
+
 }
