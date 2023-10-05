6c6ee532eb48232e750ea85d85138e9f498eb5e9
Commando <moutasem.amin@hotmail.com>
2023-10-05 20:33:16 +0200

    Create elderly class

---
+++ app/src/main/java/com/example/eldercare/ElderlyOverview.java
@@ -0,0 +1,17 @@
+package com.example.eldercare;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ElderlyOverview extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         // Set current activity to ac
+        setContentView(R.layout.activity_elderly);
+
+    }
+}
