a9c2de50493d00d480f8012ed44c1ef65c594365
Oscar Stenqvist <oscar@Oscars-MacBook-Pro-2.local>
2023-10-06 17:19:01 +0200

    prep for merge

---
+++ app/src/main/java/com/example/eldercare/FireBaseMessageReceiver.java
@@ -0,0 +1,48 @@
+package com.example.eldercare;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FireBaseMessageReciever extends FirebaseMessagingService {
+
+    // Override onNewToken to get new token
+    @Override
+    public void onNewToken(@NonNull String token) {
+        Log.d("CREATION", "Refreshed token: " + token);
+    }
+
+    // Override onMessageReceived() method to extract the
+    // title and
+    // body from the message passed in FCM
+    @Override
+    public void
+    onMessageReceived(RemoteMessage remoteMessage)
+    {
+        // First case when notifications are received via
+        // data event
+        // Here, 'title' and 'message' are the assumed names
+        // of JSON
+        // attributes. Since here we do not have any data
+        // payload, This section is commented out. It is
+        // here only for reference purposes.
+        /*if(remoteMessage.getData().size()>0){
+            showNotification(remoteMessage.getData().get("title"),
+                          remoteMessage.getData().get("message"));
+        }*/
+
+        // Second case when notification payload is
+        // received.
+        if (remoteMessage.getNotification() != null) {
+            // Since the notification is received directly
+            // from FCM, the title and the body can be
+            // fetched directly as below.
+            showNotification(
+                    remoteMessage.getNotification().getTitle(),
+                    remoteMessage.getNotification().getBody());
+        }
+    }
+}
