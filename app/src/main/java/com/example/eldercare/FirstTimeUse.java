7087a50eb858cdf7e46fc489a7e0cedc831cfba0
Commando <moutasem.amin@hotmail.com>
2023-10-06 11:42:27 +0200

    Add elderlyOverView Class some intent]

--- app/src/main/java/com/example/eldercare/FirstTimeUse.java
+++ app/src/main/java/com/example/eldercare/FirstTimeUse.java
@@ -1,9 +1,5 @@
 package com.example.eldercare;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,22 +7,17 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 public class FirstTimeUse extends AppCompatActivity {
 
@@ -100,7 +91,7 @@ public class FirstTimeUse extends AppCompatActivity {
                                         public void onComplete(@NonNull Task<AuthResult> task) {
                                             if (task.isSuccessful()) {
                                                 setFirstTimeUsePreferences(false, false, elderlyMail);
-                                                Intent intent = new Intent(getApplicationContext(), Login.class);
+                                                Intent intent = new Intent(getApplicationContext(), ElderlyOverview.class);
                                                 startActivity(intent);
                                                 finish();
                                             } else {
