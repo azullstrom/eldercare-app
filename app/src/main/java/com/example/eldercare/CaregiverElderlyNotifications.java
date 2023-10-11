be88b8de3dbb3204db10e342622c97af2cf75058
Oscar Stenqvist <oscar@Oscars-MacBook-Pro-2.local>
2023-10-11 13:16:24 +0200

    Added notificationhistory

---
+++ app/src/main/java/com/example/eldercare/CaregiverElderlyNotifications.java
@@ -0,0 +1,14 @@
+package com.example.eldercare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CaregiverElderlyNotifications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caregiver_elderly_notifications);
+    }
+}
