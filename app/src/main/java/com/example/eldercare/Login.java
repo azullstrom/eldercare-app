a8189d80dae909b0643fd24bf3a3f18970b5f8d6
Oscar Stenqvist <oscar@Oscars-MacBook-Pro-2.local>
2023-10-13 11:35:36 +0200

    Added time edit of meals

--- app/src/main/java/com/example/eldercare/Login.java
+++ app/src/main/java/com/example/eldercare/Login.java
@@ -23,7 +23,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
 
-    private static final boolean TEST_MODE = false;
+    private static final boolean TEST_MODE = true;
     Button loginButton;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
